6355a044b8a70d3d8c5ffc9d269582bd8f912e85
Evan Busse <evanbusse@gmail.com>
2017-10-09 15:42:08 -0600

    added first component: wrapper

--- cuisine-machine/src/App.js
+++ cuisine-machine/src/App.js
@@ -3,19 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+// Components
+import Wrapper from './components/Wrapper'
+
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <Wrapper/>
       </MuiThemeProvider>
     );
   }
---
+++ cuisine-machine/src/components/Wrapper.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Wrapper extends Component {
+  render(){
+    return (
+      <p>Hello world</p>
+    )
+  }
+}
+
+export default Wrapper
