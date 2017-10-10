6b527f77aa8e80306523245704153043c0249255
Evan Busse <evanbusse@gmail.com>
2017-10-09 20:13:15 -0600

    working on intro form section

--- cuisine-machine/src/components/GroupForm.js
+++ cuisine-machine/src/components/GroupForm.js
@@ -9,6 +9,16 @@ var style = {
     height: '100vh',
     // boxShadow: '0 0 4px 0 rgba(0, 0, 0, 0.3)',
     padding: 5,
+  },
+  form_section: {
+    margin: '20px 0 0 0',
+    borderTop: '2px solid #9756A2',
+    paddingTop: 10,
+    
+  },
+  input: {
+    margin: 5,
+
   }
 }
 
@@ -16,19 +26,25 @@ class GroupForm extends Component {
   render() {
     return (
       <div style={style.main_form}>
-      <p>We need a little information about you to get started. We only store your data as long as your group is active. Once you reach a decision all the information is deleted.</p>
-      <TextField
-        hintText="Name"
-        fullWidth={true}
-      />
-      <TextField
-        hintText="Email"
-        fullWidth={true}
-      />
-      <TextField
-        hintText="Phone"
-        fullWidth={true}
-      />
+      <p>We need a little information about you to get started.</p>
+      <p>Information is only stored as long as your group is active. Once you reach a decision all the information is deleted.</p>
+        <div style={style.form_section}>
+          <TextField
+            style={style.input}
+            hintText="Name"
+            fullWidth={true}
+          />
+          <TextField
+            style={style.input}
+            hintText="Email"
+            fullWidth={true}
+          />
+          <TextField
+            style={style.input}
+            hintText="Phone"
+            fullWidth={true}
+        />
+        </div>
       </div>
     )
   }
